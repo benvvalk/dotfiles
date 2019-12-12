de446820364fff09a15861e23c8d34d866fb4399
Ben Vandervalk <benv@bcgsc.ca>
2019-12-12 15:46:57 -0500

    emacs-redux: start emacs config from scratch (again!)
    The goals of redoing my emacs config are:
    
    * to have a config that I understand "from the ground up" (unlike spacemacs)
    * to have a config that does *not* require network access or a complicated
    initial setup process
    * to have a config with a fast startup time
    * to have a config that is simple, minimal, and easy to change
    
    In my previous emacs config (`emacs-reboot`), I installed packages by manually
    cloning GitHub repos and merging them into this repo with `git subtree add`.
    This approach was too tedious and error-prone, because I had to manually keep
    track of emacs package dependencies and emacs package versions.
    
    This time, I will use `package.el` to install packages, like most emacs users.
    However, I will add the files of installed packages to this git repo, so that I
    only have to do a `git clone` of this repo in order to setup the same config
    on another machine.

---
+++ emacs-redux/.emacs.d/init.el
@@ -0,0 +1,40 @@
+;;----------------------------------------
+;; basic UI settings
+;;----------------------------------------
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(menu-bar-mode -1)
+
+;;----------------------------------------
+;; package.el
+;;----------------------------------------
+
+(require 'package)
+
+;; Don't auto-load installed packages
+;; after init.el finishes. Let `use-package`
+;; handle loading of packages instead (see
+;; below).
+(setq package-enable-at-startup nil)
+
+;; Uncomment the following line to work around
+;; package signing problems
+
+;;(setq package-check-signature nil)
+
+(setq package-archives '(("org"   . "https://orgmode.org/elpa/")
+                         ("gnu"   . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+(package-initialize)
+
+;;----------------------------------------
+;; use-package
+;;----------------------------------------
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
