6ba5d27ddf12280ac439c3e04f28e377151dfa48
Ben Vandervalk <ben.vvalk@gmail.com>
2021-04-11 14:47:07 -0400

    piglet.el: implement piglet-viewer-launch-unity-project [new]
    Change main function from `piglet-viewer-create-package` ->
    `piglet-viewer-launch-unity-project`.
    
    Update elisp code to call `piglet-viewer.mk` instead of
    `create-piglet-viewer-package`. (The former has replaced the latter.)
    
    Note to self: These scripts come from my bitbucket/piglet-scripts
    repo.

--- emacs-redux/.emacs.d/site-lisp/piglet/piglet.el
+++ emacs-redux/.emacs.d/site-lisp/piglet/piglet.el
@@ -2,43 +2,42 @@
 
 (setq piglet-git-dir "/mnt/c/Users/Ben/git/bitbucket/piglet")
 (setq piglet-viewer-git-dir "/mnt/c/Users/Ben/git/awesomesaucelabs/piglet-viewer")
-(setq piglet-package-dir "/mnt/d/tmp/piglet/package")
-(setq piglet-temp-dir "/mnt/d/tmp")
+
+(defun piglet-shell-command-to-lines (command)
+  "Return the output of a shell command as a list of lines."
+  (split-string (shell-command-to-string command) "\n"))
 
 (defun piglet-git-tags-and-branches (git-dir)
   "Return a list of git tags and git branches for the repo in GIT-DIR."
   (let ((default-directory git-dir))
     (append
-     (split-string (shell-command-to-string "git tag") "\n")
-     (split-string (shell-command-to-string
-                    "git branch --format='%(refname:short)'") "\n"))))
+     (piglet-shell-command-to-lines "git tag")
+     (piglet-shell-command-to-lines "git branch --format='%(refname:short)'") "\n")))
 
-(defun piglet-viewer-create-package ()
-  "Interactive command to create a .unitypackage for piglet-viewer.
-Prompts for a piglet commit, a piglet-viewer commit, and an output
-path for the resulting .unitypackage file."
+(defun piglet-unity-version-list ()
+  "Return a list of installed Unity versions."
+  (piglet-shell-command-to-lines "unity-list-version"))
+
+(defun piglet-viewer-launch-unity-project ()
+  "Interactive command to create and open a Unity project containing
+Piglet and PigletViewer."
   (interactive)
-  (let* ((piglet-commit (completing-read
+  (let* ((unity-version (completing-read
+                         "unity version: "
+                         (piglet-unity-version-list)))
+         (piglet-commit (completing-read
                          "piglet commit: "
                          (piglet-git-tags-and-branches
                           piglet-git-dir)))
          (piglet-viewer-commit (completing-read
                                 "piglet-viewer commit: "
                                 (piglet-git-tags-and-branches
-                                 piglet-viewer-git-dir)))
-         (default-package-name (format
-                                "piglet-viewer-%s.unitypackage"
-                                piglet-viewer-commit))
-         (output-file (read-file-name
-                       "output file: "
-                       piglet-package-dir
-                       default-package-name)))
+                                 piglet-viewer-git-dir))))
     (async-shell-command
-     (format (concat "create-piglet-viewer-package -v "
-                     "--piglet-commit '%s' "
-                     "--piglet-viewer-commit '%s' "
-                     " --temp-dir '%s' --output '%s'")
+     (format (concat "piglet-viewer.mk "
+                     "unity-version='%s' "
+                     "piglet-commit='%s' "
+                     "piglet-viewer-commit='%s'")
+             unity-version
              piglet-commit
-             piglet-viewer-commit
-             piglet-temp-dir
-             output-file))))
+             piglet-viewer-commit))))
