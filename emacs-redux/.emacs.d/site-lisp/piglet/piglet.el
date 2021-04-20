ea4b94a63456130d796ad20a7179e57048945ce5
Ben Vandervalk <ben.vvalk@gmail.com>
2021-04-20 09:43:57 -0400

    piglet.el: add piglet-viewer-launch-win64 [new]

--- emacs-redux/.emacs.d/site-lisp/piglet/piglet.el
+++ emacs-redux/.emacs.d/site-lisp/piglet/piglet.el
@@ -18,7 +18,7 @@
   "Return a list of installed Unity versions."
   (piglet-shell-command-to-lines "unity-list-version"))
 
-(defun piglet-viewer-launch-unity-project ()
+(defun piglet-viewer-launch-unity ()
   "Interactive command to create and open a Unity project containing
 Piglet and PigletViewer."
   (interactive)
@@ -32,12 +32,47 @@ Piglet and PigletViewer."
          (piglet-viewer-commit (completing-read
                                 "piglet-viewer commit: "
                                 (piglet-git-tags-and-branches
-                                 piglet-viewer-git-dir))))
+                                 piglet-viewer-git-dir)))
+         (buffer-name (format "*piglet-viewer-%s/piglet-%s/unity-%s*"
+                              piglet-commit
+                              piglet-viewer-commit
+                              unity-version)))
     (async-shell-command
      (format (concat "piglet-viewer.mk "
                      "unity-version='%s' "
                      "piglet-commit='%s' "
-                     "piglet-viewer-commit='%s'")
+                     "piglet-viewer-commit='%s' "
+                     "launch-unity")
              unity-version
              piglet-commit
-             piglet-viewer-commit))))
+             piglet-viewer-commit)
+     buffer-name)))
+
+(defun piglet-viewer-launch-win64 ()
+  "Interactive command to build and run PigletViewer on Windows (64-bit)."
+  (interactive)
+  (let* ((unity-version (completing-read
+                         "unity version: "
+                         (piglet-unity-version-list)))
+         (piglet-commit (completing-read
+                         "piglet commit: "
+                         (piglet-git-tags-and-branches
+                          piglet-git-dir)))
+         (piglet-viewer-commit (completing-read
+                                "piglet-viewer commit: "
+                                (piglet-git-tags-and-branches
+                                 piglet-viewer-git-dir)))
+         (buffer-name (format "*win64/piglet-viewer-%s/piglet-%s/unity-%s*"
+                              piglet-commit
+                              piglet-viewer-commit
+                              unity-version)))
+    (async-shell-command
+     (format (concat "piglet-viewer.mk "
+                     "unity-version='%s' "
+                     "piglet-commit='%s' "
+                     "piglet-viewer-commit='%s' "
+                     "launch-win64-exe")
+             unity-version
+             piglet-commit
+             piglet-viewer-commit)
+     buffer-name)))
