61e18ec555469ef32a3d29b8f80f785d62340e1b
Ben Vandervalk <ben.vvalk@gmail.com>
2021-04-26 14:03:11 -0400

    emacs-redux: add ~/.emacs.d/custom.el [new]
    custom.el is the file where the lisp code generated by the emacs
    customization system gets written.
    
    After learning a little bit about the emacs customization system, I
    now realize that it is safe (and a good idea) to put custom.el under
    version control.  Also, I'm getting a bit tired of always seeing
    custom.el in my list of unstaged git changes.

---
+++ emacs-redux/.emacs.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(winum vterm use-package orgit org-roam org-cliplink omnisharp markdown-mode lispy gnu-elpa-keyring-update general evil-surround evil-collection elfeed counsel-projectile company calfw-org calfw ace-link)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
