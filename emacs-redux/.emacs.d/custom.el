256ce384a11e6d19ac2c19b820fd0743cd5d7eed
Ben Vandervalk <ben.vvalk@gmail.com>
2021-05-03 09:00:16 -0400

    emacs: add `request` package (missing counsel-web dependency) [bugfix]
    Counsel-web is still not working on my Windows desktop machine for
    some reason.  I get an error related to xml parsing, but I'll look at
    it later.

--- emacs-redux/.emacs.d/custom.el
+++ emacs-redux/.emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ledger-mode winum vterm use-package orgit org-roam org-cliplink omnisharp markdown-mode lispy gnu-elpa-keyring-update general evil-surround evil-collection elfeed counsel-projectile company calfw-org calfw ace-link)))
+   '(request ledger-mode winum vterm use-package orgit org-roam org-cliplink omnisharp markdown-mode lispy gnu-elpa-keyring-update general evil-surround evil-collection elfeed counsel-projectile company calfw-org calfw ace-link)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
