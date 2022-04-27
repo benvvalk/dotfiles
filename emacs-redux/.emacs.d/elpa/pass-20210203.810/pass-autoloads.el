9ab896f2599f7518c26f7b941ada576d1a94b452
Ben Vandervalk <ben.vvalk@gmail.com>
2022-04-27 18:18:25 -0400

    emacs: reinstall `pass` package [cleanup]
    The first time I installed the `pass` package, I forgot to commit its
    dependencies (the `package-store` and `package-store-otp` packages).
    
    To fix this, I ran (package-reinstall "pass") in emacs.

--- emacs-redux/.emacs.d/elpa/pass-20210203.810/pass-autoloads.el
+++ emacs-redux/.emacs.d/elpa/pass-20210203.810/pass-autoloads.el
@@ -6,13 +6,26 @@
                          (or (file-name-directory #$) (car load-path))))
 
 
-;;;### (autoloads nil "pass" "pass.el" (0 0 0 0))
-;;; Generated autoloads from pass.el
+;;;### (autoloads nil "pass" "../../../../../.emacs.d/elpa/pass-20210203.810/pass.el"
+;;;;;;  "eab4a3d9793964733d3d9acddd417b0a")
+;;; Generated autoloads from ../../../../../.emacs.d/elpa/pass-20210203.810/pass.el
 
 (autoload 'pass "pass" "\
 Open the password-store buffer." t nil)
+
+;;;### (autoloads "actual autoloads are elsewhere" "pass" "../../../../../.emacs.d/elpa/pass-20210203.810/pass.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../.emacs.d/elpa/pass-20210203.810/pass.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "pass" '("pass-")))
+
+;;;***
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/pass-20210203.810/pass-autoloads.el"
+;;;;;;  "../../../../../.emacs.d/elpa/pass-20210203.810/pass.el")
+;;;;;;  (0 0 0 0))
 
 ;;;***
 
