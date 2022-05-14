bc57a818012ad2d9f37e55cf2d8e91fca624bdd4
Ben Vandervalk <ben.vvalk@gmail.com>
2022-05-14 17:40:54 -0400

    emacs: install `consult` package [new]

---
+++ emacs-redux/.emacs.d/elpa/consult-20220508.928/consult-pkg.el
@@ -0,0 +1,11 @@
+(define-package "consult" "20220508.928" "Consulting completing-read"
+  '((emacs "27.1")
+    (compat "28.1"))
+  :commit "1dfdf55f3d941f08089e5d0e611cd9daa8a44b19" :authors
+  '(("Daniel Mendler and Consult contributors"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :url "https://github.com/minad/consult")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
