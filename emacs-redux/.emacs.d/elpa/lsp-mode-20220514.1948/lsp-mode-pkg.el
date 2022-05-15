6e5c7c931069b2112d348a5e25bef84f580d4131
Ben Vandervalk <ben.vvalk@gmail.com>
2022-05-14 22:29:27 -0400

    emacs: upgrade `lsp-mode` package [upgrade]

---
+++ emacs-redux/.emacs.d/elpa/lsp-mode-20220514.1948/lsp-mode-pkg.el
@@ -0,0 +1,18 @@
+(define-package "lsp-mode" "20220514.1948" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.3")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "1d9ec3f30a6491b58a904b071694a1a8a0faca87" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
