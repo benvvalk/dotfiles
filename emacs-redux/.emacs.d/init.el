60d5f9a4adb2f5200a430fa599ad5334c80d00c6
Ben Vandervalk <ben.vvalk@gmail.com>
2023-11-25 10:30:15 -0500

    emacs: enable `magit-copy-revision-abbreviated` [tweak]
    Replace custom code with `magit-copy-revision-abbreviated`
    setting. (In previous versions of magit, this setting did not exist,
    so I had to implement it myself.)

--- emacs-redux/.emacs.d/init.el
+++ emacs-redux/.emacs.d/init.el
@@ -1471,15 +1471,10 @@ returns exit status 1, for reasons I don't understand.
   (aset buffer-display-table ?\^M []))
 
 (use-package magit
-  :config
-  ;; Hack `magit-copy-section-value` with an advice, so that copying
-  ;; a commit in a magit buffer (keybinding `y s`) copies the
-  ;; abbreviated 7-character commit ID rather than the full ID.
-  (defun benv/copy-section-value-abbrev ()
-    (let ((result (substring (car kill-ring-yank-pointer) 0 7)))
-      (kill-new result)
-      (message result)))
-  (advice-add 'magit-copy-section-value :after #'benv/copy-section-value-abbrev)
+
+  :custom
+  (magit-copy-revision-abbreviated t)
+
   :general
     ;; Unset any bindings for my evil leader
     ;; key (currently "SPC") in magit, so that
