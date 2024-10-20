8b9dfa31d8974def342137ded744b987cc255ba7
Ben Vandervalk <ben.vvalk@gmail.com>
2024-10-20 19:21:35 -0400

    nixos: add missing `};` to flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -11,6 +11,7 @@
         home-manager = {
             url = "github:nix-community/home-manager/release-24.05";
             inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
     outputs = { nixpkgs, home-manager, ... }:
