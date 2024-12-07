60e721f060a9e360b25d3e4f11c60ecabed14f79
Ben Vandervalk <ben.vvalk@gmail.com>
2024-12-07 17:57:38 -0500

    emacs: upgrade gptel (and transient)
    I wanted to upgrade gptel because it's now got an improved UI and
    support for media files (e.g. images).
    
    Note: After upgrading `gptel` alone, I started to get a
    transient-related error when running `gptel-menu`, as discussed in
    https://github.com/karthink/gptel/issues/348:
    
    ```
    transient-setup: Wrong type argument: (or eieio-object cl-structure-object oclosure), ""
    ```
    
    In order to fix the above error, I needed to upgrade `transient`
    package as well. However, since `transient` (version 0.4.3) is a
    built-in package for Emacs 29.4, `transient` is not shown in the
    completion list when running M-x `package-upgrade`. To force emacs to
    upgrade a built-in package like `transient`, I needed to run
    `package-upgrade` with the prefix argument.

---
+++ emacs-redux/.emacs.d/elpa/gptel-20241207.1916/gptel-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "gptel" "20241207.1916"
+  "Interact with ChatGPT or other LLMs."
+  '((emacs     "27.1")
+    (transient "0.4.0")
+    (compat    "29.1.4.1"))
+  :url "https://github.com/karthink/gptel"
+  :commit "ba07a52e73f8a6cbce6b94b8628ddcb9c0677330"
+  :revdesc "ba07a52e73f8"
+  :keywords '("convenience")
+  :authors '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainers '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")))
