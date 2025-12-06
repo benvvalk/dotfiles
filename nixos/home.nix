8381ffefddedbfcc4f2b4dc89ea92c676d188448
Ben Vandervalk <ben.vvalk@gmail.com>
2025-12-06 12:03:39 -0500

    home.nix: add syncthing service, with helpful comments

--- nixos/home.nix
+++ nixos/home.nix
@@ -40,4 +40,32 @@
             # if [ ! -e .emacs.d -a -d dotfiles/emacs-redux ]; then stow --dir=dotfiles emacs-redux; fi
         '';
     };
+
+    # Automatically start Syncthing as a user-level systemd service.
+    #
+    # Note: I need to do the initial setup of Syncthing manually:
+    #
+    # (1) On my Mac M1 Mini, share the "~/Sync" folder to this machine.
+    # (2) On this machine, approve connection with Mac M1 Mini and accept
+    #     its offer to share the "~/Sync" folder.
+    # (3) On this machine, go to the web UI (localhost:8384) and
+    #     set the GUI user/password. Use the user/password from
+    #     my password store so that I won't forget it.
+    #
+    # Previously, I tried making a very fancy/automated Syncthing setup as a
+    # system-level service in `configuration.nix`, but I found that it was
+    # lacking some important options, and the extra complexity really wasn't
+    # worth the benefits. For example, it doesn't seem possible to safely set
+    # the GUI user/password via `sops-nix`, because it can only read the
+    # user/password attributes from a string rather than a file.
+    #
+    # My abandoned attempt to make an automated Syncthing setup is on the
+    # `nixos/syncthing-as-system-service` branch of my `dotfiles` repo.
+    services.syncthing = {
+       enable = true;
+       # Don't create an empty `~/Sync` folder by default. When initially
+       # syncing folders from my Mac M1 Mini, it feels safer and clearer
+       # if there isn't already an (empty) `~/Sync` folder on this machine.
+       extraOptions = ["--no-default-folder"];
+    };
 }
