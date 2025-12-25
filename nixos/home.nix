6115bff754a39af7c583594da8d1d831f2f32952
Ben Vandervalk <ben.vvalk@gmail.com>
2025-12-25 12:02:57 -0500

    home.nix: install `gh` (GitHub CLI)

--- nixos/home.nix
+++ nixos/home.nix
@@ -22,6 +22,7 @@
             filezilla # graphical FTP client
             gcc # `org-roam` needs this to auto-compile its own `sqlite` binary
             gcr # added because of advice here: https://mynixos.com/home-manager/option/services.gpg-agent.pinentry.package
+            gh # GitHub CLI tool
             gnome-terminal
             (pass.withExtensions (exts: [exts.pass-otp])) # copied from: https://b2g.h11e.de/2024/07/pass-ext/
             postgresql # for `sponsoredissues.org` development (see note above)
