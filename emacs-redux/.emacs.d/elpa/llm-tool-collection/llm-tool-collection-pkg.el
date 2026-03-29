b324848b5360319ce78ca4b97a758ee7277b6cb6
Ben Vandervalk <ben.vvalk@gmail.com>
2026-03-29 12:59:26 -0400

    emacs: install `llm-tool-collection` b9fd45b (for `gptel`) [feat]
    Install `llm-tool-collection` using `vc-package-install` and entering
    `https://github.com/skissue/llm-tool-collection.git` at the minibuffer
    prompt.
    
    Then manually delete the `.emacs.d/elpa/llm-tool-collection/.git` dir
    that `vc-package-install` created. (Git ignores subdirectories that
    have their own `.git` directories. For example, see the post at [1].)
    
    [1]: https://unix.stackexchange.com/a/323490

---
+++ emacs-redux/.emacs.d/elpa/llm-tool-collection/llm-tool-collection-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from llm-tool-collection.el  -*- no-byte-compile: t -*-
+(define-package "llm-tool-collection" "0.1.0" "No description available." '((emacs "28.1")) :kind vc :commit "b9fd45bedf3e0fb07d289730991199ae18785157")
